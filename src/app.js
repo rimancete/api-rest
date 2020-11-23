5cb68e97468f8f047704807d6487f530dac4b9d3
Osvaldo Costa <osvaldo.costajr@gmail.com>
2020-11-23 17:51:32 -0300

    CORS added

--- src/app.js
+++ src/app.js
@@ -6,11 +6,32 @@ dotenv.config();
 import './database';
 
 import express from 'express';
+// ajustes para comunicação com React => permitindo quem pode acessar nossa api
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import photoRoutes from './routes/photoRoutes';
+// lista de domínios permitidos
+const whiteList = [
+  'https://react2.otaviomiranda.com.br',
+  'http://localhost:3000',
+];
+// configurações do cors => origin site origem que está tentando acessar nossa api(permitidos em nossa whitelist)
+const corsOptions = {
+  origin(origin, callback) {
+    // se a origem tiver algum valor da whiteList ou não for preenchida
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+      // se n~ao for domínio permitido
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,6 +41,9 @@ class App {
   }
 
   middlewares() {
+    // utilizando cors e helmet
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     // setar pasta de arquivos estáticos
